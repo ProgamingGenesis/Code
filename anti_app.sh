9906ed01695a4886ca9c54102997c3588d8a0322
brgvefdsc <91153971+brgvefdsc@users.noreply.github.com>
2021-12-09 19:31:54 -0800

    Update anti_app.sh

--- anti_app.sh
+++ anti_app.sh
@@ -1,3 +1,5 @@
+# a little bash script for mac that stops an app from starting. just a little prank script I use on technicly challenged friends
+#USAGE: ./anti_app.sh [APP]
 while :
 do
   pkill $1
