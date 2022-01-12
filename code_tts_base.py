51c4ce653060d924546876ffbbb3db56f263fa8c
brgvefdsc <91153971+brgvefdsc@users.noreply.github.com>
2022-01-12 09:40:34 -0800

    Add files via upload

--- code_tts_base.py
+++ code_tts_base.py
@@ -1,3 +1,12 @@
+from os import posix_spawn
+
+def class_get (x, usr_input):
+    try:
+        x(usr_input)
+        return True
+    except:
+        return False
+
 def ls_indexor (string):
     try:
         i = ls_txt.index(string)
@@ -20,6 +29,12 @@ while True:
         if ls_txt[ls_indexor("set") + 1] == "variable":
             name = ls_txt[ls_indexor("variable") + 1] + " = "
             value = ls_txt[ls_indexor("to") + 1]
-            cmd = name + value
-            with open(file_name, 'w') as f:
-                f.write(cmd)        
+            if class_get(int, value) == True:
+                cmd = name + value
+                with open(file_name, 'w') as f:
+                    f.write(cmd)   
+            if class_get(str, value) == True:
+                cmd = name + " '" + value + "' "
+                with open(file_name, 'w') as f:
+                    f.write(cmd)   
+    
